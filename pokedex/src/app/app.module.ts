b4b95448e72a591f984a3ada8fcac49cfbe2e848
Donzellini <muriel.donzellini@gmail.com>
2022-02-04 21:19:18 -0300

    creating components

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokedexCardComponent } from './pokedex-card/pokedex-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent
+    PokemonListComponent,
+    PokedexCardComponent
   ],
   imports: [
     BrowserModule,
